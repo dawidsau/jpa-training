bb6c5bf218123e80c194c8594821395e441c3525
sdakf <sdakftraining@gmail.com>
2018-03-18 12:09:15 +0100

    persistence.xml

--- src/main/java/pl/sda/jpatraining/jpa/Customer.java
+++ src/main/java/pl/sda/jpatraining/jpa/Customer.java
@@ -1,11 +1,19 @@
 package pl.sda.jpatraining.jpa;
 
+import javax.persistence.*;
+
+@Entity
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "first_name")
     private String firstName;
     private String surname;
     private String city;
+    @Column(name = "postal_code")
     private String postalCode;
     private String street;
-    
+
 }
